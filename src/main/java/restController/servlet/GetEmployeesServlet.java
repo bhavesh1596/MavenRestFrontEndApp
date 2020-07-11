3be007036dc2872def09259eef34a27bfd825580
bhavesh <bhaveshpopli.bp@gmail.com>
2020-07-11 20:54:24 +0530

    Created basic maven JSP Servlet Project

---
+++ src/main/java/restController/servlet/GetEmployeesServlet.java
@@ -0,0 +1,25 @@
+package restController.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class GetEmployeesServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException  {
+		System.out.println("getEmpServlet Request Method : "+req.getMethod());
+	}
+
+	
+
+}
